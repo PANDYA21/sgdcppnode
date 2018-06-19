56b6a86886a8d7013ee3e0b7829b2e45144b3ee9
Bhaumik Pandya <bar@example.com>
2018-06-19 16:05:15 +0200

    clean up

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./build/Release/native.node').sgd;
+const sgd = require('./build/Release/native.node').sgd;
+
+function sgdWrapper(params) {
+	return sgd(
+		params.input,
+		params.output,
+		params.learning_rate,
+		params.maxiter,
+		params.minerr,
+		params.verbose 
+	);
+}
+
+module.exports = sgdWrapper;
--- test.js
+++ test.js
@@ -5,28 +5,21 @@ const _ = require('lodash');
 const nrows = 10; // data size
 let xt = [];
 let yt = [];
-const m = 2;
-const c = 3;
+const m = 20; // slope of the line
+const c = 3; // intersection with x-axis
+
 for (let i = 1; i < nrows; i++) {
 	xt.push(i / Math.pow(nrows, 1));
 	yt.push((m * i) / Math.pow(nrows, 1) + c);
 }
 
-// console.log(xt, yt);
-
-const learning_rate = 0.3;
-const maxiter = 500;
-const minerr = 1e-15;
-
-const slopes = sgd(
-	// xt,
-	// yt,
-	[0, 0.1, 0.2, 0.3],
-	[0, 0.2, 0.4, 0.6],
-	learning_rate,
-	maxiter,
-	minerr,
-	false //verbose
-);
+const slopes = sgd({
+	input: xt,
+	output: yt,
+	learning_rate: 0.3, 
+	maxiter: 1e5,
+	minerr: 1e-13,
+	verbose: false
+});
 
 console.log(slopes);
