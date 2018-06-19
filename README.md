920674a230ef5691ca539abb40c5e72a1129ac04
Bhaumik Pandya <bar@example.com>
2018-06-19 17:51:49 +0200

    benchmark and readme

--- README.md
+++ README.md
@@ -3,18 +3,42 @@ This module is implemented on C++ based [sgd optimizer](https://en.wikipedia.org
 
 ## Why
 ### Benchmark
+#### Single Attempt
 Weights to learn:
 ```js
+const ita = 0.3;
+const maxiter = 1e5;
+const minerr = 1e-5;
+const verbose = false;
+const attempts = 1; // number of performance checks
+
+// weights to learn and data size
 const w1 = 0.5;
 const w2 = 2;
+const nrows = 100;
 ```
 
 Outcome:
 ```js
-Iterations required:  100000000
-Learned weights with JS:  { w1: 0.31361413912488656, w2: 1.961709566226222 }
-Processing time (ms):  119777
-Learned weights with CPP:  { w1: 0.40295447227588255, w2: 1.2320685656171875 }
-Processing time (ms):  84439
-Required iterations: 100000000
+Iterations required:  100000
+Learned weights with JS:  { w1: 0.27534322030308456, w2: 1.8251252795222055 }
+Processing time (ms):  141
+Learned weights with CPP:  { w1: 0.4085977705077681, w2: 1.451816311362102 }
+Processing time (ms):  85
+
+
+Average JS time:  141
+Average CPP time:  85
+
+
+Required iterations: 100000
 ```
+
+#### Multiple Attempts
+With the same parameters except `const attempts = 1;`, the outcome is:
+```js
+Average JS time:  130.52
+Average CPP time:  84.54
+```
+
+CPP turns out to be approximately <b style="color: lightgreen; background-color: black;">35%</b> faster than JS!
--- benchmark.js
+++ benchmark.js
@@ -6,8 +6,7 @@ const ita = 0.3;
 const maxiter = 1e5;
 const minerr = 1e-5;
 const verbose = false;
-
-const attempts = 5; // 5 performance checks
+const attempts = 50; // number of performance checks
 
 const w1 = 0.5;
 const w2 = 2;
@@ -16,60 +15,56 @@ const { x, y } = generateSyntheticData(w1, w2, nrows, false);
 
 let benchmarks = [];
 
-function testJs(next) {
-	for (let i = 0; i < attempts; i++) {
-		const slopes_js = sgdJs({
-			maxiter,
-			input: x,
-			output: y,
-			learning_rate: ita,
-			minerr,
-			verbose
-		});
+function testJs(attempt, next) {
+	const slopes_js = sgdJs({
+		maxiter,
+		input: x,
+		output: y,
+		learning_rate: ita,
+		minerr,
+		verbose
+	});
 
-		console.log('Learned weights with JS: ', slopes_js);
-		next((start, end, difference) => {
-			console.log('Processing time (ms): ', difference.milliseconds);
-			benchmarks.push({
-				attempt: i,
-				type: 'JS',
-				milliseconds: difference.milliseconds,
-				difference
-			});
+	console.log('Learned weights with JS: ', slopes_js);
+	next((start, end, difference) => {
+		console.log('Processing time (ms): ', difference.milliseconds);
+		benchmarks.push({
+			attempt,
+			type: 'JS',
+			milliseconds: difference.milliseconds,
+			difference
 		});
-	}
+	});
 }
 
 
-function testCpp(next) {
-	for (let i = 0; i < attempts; i++) {
-		const slopes_cpp = sgdCpp({
-			input: x,
-			output: y,
-			learning_rate: ita,
-			maxiter,
-			minerr,
-			verbose
-		});
+function testCpp(attempt, next) {
+	const slopes_cpp = sgdCpp({
+		input: x,
+		output: y,
+		learning_rate: ita,
+		maxiter,
+		minerr,
+		verbose
+	});
 
-		console.log('Learned weights with CPP: ', slopes_cpp);
-		next((start, end, difference) => {
-			console.log('Processing time (ms): ', difference.milliseconds);
-			benchmarks.push({
-				attempt: i,
-				type: 'CPP',
-				milliseconds: difference.milliseconds,
-				difference
-			});
+	console.log('Learned weights with CPP: ', slopes_cpp);
+	next((start, end, difference) => {
+		console.log('Processing time (ms): ', difference.milliseconds);
+		benchmarks.push({
+			attempt,
+			type: 'CPP',
+			milliseconds: difference.milliseconds,
+			difference
 		});
-	}
+	});
 }
 
 
-function benchmark(method) {
+function benchmark(attempt, method) {
 	let start = +(new Date);
 
-	method && method(function(callback) {
+	method && method(attempt, function(callback) {
 		let end = +(new Date);
 		let difference = end - start;
 		callback && callback(start, end, {
@@ -82,15 +77,17 @@ function benchmark(method) {
 	});
 }
 
-benchmark(testJs);
-benchmark(testCpp);
+for (let i = 0; i < attempts; i++) {
+	benchmark(i, testJs);
+	benchmark(i, testCpp);
+}
 
 // console.log(JSON.stringify(benchmarks, null, 2));
 
-const avg_js = _.sum(_.map(_.filter(benchmarks, { type: 'JS' }), 'milliseconds'))/attempts;
-const avg_cpp = _.sum(_.map(_.filter(benchmarks, { type: 'CPP' }), 'milliseconds'))/attempts;
+const avg_js = _.sum(_.map(_.filter(benchmarks, { type: 'JS' }), 'milliseconds')) / attempts;
+const avg_cpp = _.sum(_.map(_.filter(benchmarks, { type: 'CPP' }), 'milliseconds')) / attempts;
 
-console.log('\n\n');
+console.log('\n');
 console.log('Average JS time: ', avg_js);
 console.log('Average CPP time: ', avg_cpp);
-console.log('\n\n');
+console.log('\n');
